8a3eea84246b5ccf9224cf7d66eb392fa1c99896
BRHeaven <brheaven872@gmail.com>
2023-05-14 12:42:42 +0700

    update code of app.js, index.js and create folders, file

---
+++ src/layouts/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function Home(props) {
+  return (
+    <section>
+
+    </section>
+  )
+};
+export default connect(null,null)(Home);
